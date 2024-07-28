b8ef3c06db7be0b5d4811fa09082010568e5ccdf
Pythe <jwoleabu@gmail.com>
2024-07-28 23:13:54 +0100

    add feature to manage roles on interaction

---
+++ src/events/guildEventAdd.ts
@@ -0,0 +1,23 @@
+import { Client, Events, GuildScheduledEvent, User } from "discord.js";
+
+export default {
+	name: Events.GuildScheduledEventUserAdd,
+	async execute(guilscheduledEvent: GuildScheduledEvent, user: User) {
+        if (!guilscheduledEvent.client?.user) {
+            console.error("Client is not properly logged in.");
+            return;
+        }
+        const guild = guilscheduledEvent.guild;
+        if(!guild){
+            return;
+        }
+        const roleManager = guild.roles;
+        const jamRole = roleManager.cache.find(role => role.name === guilscheduledEvent.name);
+        if(!jamRole || jamRole.client !== guilscheduledEvent.client){
+            return;
+        }
+        const member = await guild.members.fetch(user.id)
+        member.roles.add(jamRole)
+		console.log(`${user.tag} has been added to the scheduled event ${guilscheduledEvent.name} `);
+	},
+};
---
+++ src/events/guildEventRemove.ts
@@ -0,0 +1,24 @@
+import { Client, Events, GuildScheduledEvent, User } from "discord.js";
+
+export default {
+	name: Events.GuildScheduledEventUserRemove,
+	async execute(guilscheduledEvent: GuildScheduledEvent, user: User) {
+        if (!guilscheduledEvent.client?.user) {
+            console.error("Client is not properly logged in.");
+            return;
+        }
+        const guild = guilscheduledEvent.guild;
+        if(!guild){
+            console.log("No guild");
+            return;
+        }
+        const roleManager = guild.roles;
+        const jamRole = roleManager.cache.find(role => role.name === guilscheduledEvent.name);
+        if(!jamRole || jamRole.client !== guilscheduledEvent.client){
+            return;
+        }
+        const member = await guild.members.fetch(user.id)
+        member.roles.remove(jamRole)
+		console.log(`${user.tag} has been removed from the scheduled event ${guilscheduledEvent.name} `);
+	},
+};
